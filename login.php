1a3ac5a0072eded32a65d80bc584a9c2b95484aa
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-09-14 20:59:13 +0630

    user login and register fixes for ui

--- login.php
+++ login.php
@@ -1,42 +1,47 @@
 <?php
-    session_start();
-    include("./helpers/FLUSH.php");
+session_start();
+include("./helpers/FLUSH.php");
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Home</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style>
-        .wrap {
-            width: 100%;
-            max-width: 400px;
-            margin: 40px auto;
-        }
-    </style>
+    <link rel='stylesheet' href='/gwsc/asset/css/login-user.css' />
 </head>
 
 <body class="text-center">
     <div class="wrap">
-        <h1 class="h3 mb-3">Login</h1>
-        <?php if(FLUSH::check('error')): ?>
-            <div class="alert alert-warning">
-                <?= FLUSH::message('error') ?>
+        <div class='column-1'>
+            <div class='main-login'>
+                <h1 class="h3 mb-3">
+                    <div class='banner'>
+                        <img src="/gwsc/asset/images/loginPicture.jpg" class="banner"/>
+                    </div>
+                    Login
+                </h1>
+                <?php if (FLUSH::check('error')): ?>
+                    <div class="alert alert-warning">
+                        <?= FLUSH::message('error') ?>
+                    </div>
+                <?php endif; ?>
+                <form action="actions/login.php" method="post">
+                    <label class='label'>Email</label>
+                    <input type="email" name="email" class="form-control mb-2" placeholder="Email" required>
+                    <label class='label'>Password</label>
+                    <input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
+                    <button type="submit" class="w-100 btn btn-lg btn-primary">
+                        Login
+                    </button>
+                </form>
+                <br>
+                <a href="register.php">Register</a>
             </div>
-        <?php endif; ?>
-        <form action="actions/login.php" method="post">
-            <input type="email" name="email" class="form-control mb-2" placeholder="Email" required>
-            <input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
-            <button type="submit" class="w-100 btn btn-lg btn-primary">
-                Login
-            </button>
-        </form>
-        <br>
-        <a href="register.php">Register</a>
+        </div>
+
     </div>
 </body>
 
-</html>``
+</html>
--- register.php
+++ register.php
@@ -1,49 +1,53 @@
 <?php
-    session_start();
-    include("./helpers/FLUSH.php");
+session_start();
+include("./helpers/FLUSH.php");
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Register</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style>
-        .wrap {
-            width: 100%;
-            max-width: 400px;
-            margin: 40px auto;
-        }
-    </style>
+    <link rel='stylesheet' href='/gwsc/asset/css/register-user.css' />
 </head>
 
 <body class="text-center">
     <div class="wrap">
-        <h1 class="h3 mb-3">Register</h1>
-        <?php if (isset($_GET['error'])) : ?>
-            <div class="alert alert-warning">
-                Cannot create account. Please try again.
+        <div class='column-1'>
+            <div class='main-login'>
+                <h1 class="h3 mb-3">
+                    <div class='banner'>
+                        <img src="/gwsc/asset/images/loginPicture.jpg" class="banner" />
+                    </div>
+                    Register
+                </h1>
+                <?php if (FLUSH::check('error')): ?>
+                    <div class="alert alert-warning">
+                        <?= FLUSH::message('error') ?>
+                    </div>
+                <?php endif; ?>
+                <form action="actions/login.php" method="post">
+                    <label class='label'>First Name</label>
+                    <input type="text" name="first_name" class="control mb-2" placeholder="First Name" required>
+                    <label class='label'>Sur Name</label>
+                    <input type="text" name="sur_name" class="control mb-2" placeholder="Sur Name" required>
+                    <label class='label'>Email</label>
+                    <input type="email" name="email" class="control mb-2" placeholder="Email" required>
+                    <label class='label'>Phone Number</label>
+                    <input type="text" name="phone" class="control mb-2" placeholder="Phone" required>
+                    <label class='label'>Address</label>
+                    <textarea name="address" class="control mb-2" placeholder="Address" required></textarea>
+                    <label class='label'>Address</label>
+                    <input type="password" name="password" class="control mb-2" placeholder="Password" required>
+                    <button type="submit" class="w-100 btn btn-lg btn-primary">
+                        Register
+                    </button>
+                </form>
+                <br>
+                <a href="register.php">Register</a>
             </div>
-        <?php endif ?>
-        <?php if (FLUSH::check('error')) : ?>
-            <div class="alert alert-warning">
-                <?php echo FLUSH::message('error'); ?>
-            </div>
-        <?php endif ?>
-        <form action="actions/create_customer.php" method="post">
-            <input type="text" name="first_name" class="form-control mb-2" placeholder="First Name" required>
-            <input type="text" name="sur_name" class="form-control mb-2" placeholder="Sur Name" required>
-            <input type="email" name="email" class="form-control mb-2" placeholder="Email" required>
-            <input type="text" name="phone" class="form-control mb-2" placeholder="Phone" required>
-            <textarea name="address" class="form-control mb-2" placeholder="Address" required></textarea>
-            <input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
-            <button type="submit" class="w-100 btn btn-lg btn-primary">
-                Register
-            </button>
-        </form>
-        <br>
-        <a href="index.php">Login</a>
+        </div>
     </div>
 </body>
 
