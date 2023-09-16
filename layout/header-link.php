0ac5c60678c5089110c1af49c48da8fbe3cea4a6
Wai Lwin Aung <wlaung@yig.local>
2023-09-16 13:37:39 +0630

    add favicon and logo

--- layout/header-link.php
+++ layout/header-link.php
@@ -1,3 +1,11 @@
+<link rel="apple-touch-icon" sizes="180x180" href="../asset/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./asset/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./asset/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="./asset/images/favicon/site.webmanifest">
+<link rel="mask-icon" href="./asset/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="./asset/css/navbar.css" rel="stylesheet">
 <link href="./asset/css/footer.css" rel="stylesheet">
