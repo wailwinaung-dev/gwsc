e65fc89bc626514706596d08f63eb888ddb5b494
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-09-17 19:24:03 +0630

    home.js added for slide show

---
+++ asset/js/home.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides();
+showSlides2();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function showSlides2() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  // setTimeout(showSlides2, 2000); // Change image every 2 seconds
+}
