cdfee5e63f59cf08507c8db03af2cf2f8046b9c0
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-10-05 21:02:24 +0630

    privacy.php update

---
+++ privacy.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> GWSC - Privacy Policy </title>
+</head>
+
+<body>
+    <h2>
+        Introduction
+    </h2>
+    <p>
+        Welcome to GWSC ("Global Wild Swimming and Camping"). This Privacy Policy outlines how we collect, use,
+        disclose, and
+        protect your personal information when you use our Website. By accessing or using the Website, you consent to
+        the practices described in this Privacy Policy.
+    </p>
+    <h2>
+        Information We Collect
+    </h2>
+    <p>
+        We may collect both personal and non-personal information when you use our Website. Personal information may
+        include, but is not limited to:
+    <ul style="list-style-type: none;">
+        <li>Name</li>
+        <li>Email address</li>
+        <li>Phone number</li>
+        <li>Mailing address</li>
+    </ul>
+    </p>
+    <h2>
+        How We Use Your Information
+    </h2>
+    <p>
+        We use the information collected for various purposes, including but not limited to:
+
+        Providing and maintaining the Website
+        Processing your requests and transactions
+        Sending updates, newsletters, and promotional materials
+        Responding to your inquiries and requests
+        Improving our services and Website
+        Complying with legal obligations
+        Cookies and Tracking Technologies
+        We may use cookies and similar tracking technologies to collect information about your browsing activity on our
+        Website. You can control the use of cookies through your browser settings.
+    </p>
+    <h2>
+        Disclosure of Your Information
+    </h2>
+    <p>
+        We may share your personal information with third parties under the following circumstances:
+
+        With service providers and partners who assist us in providing the Website and services
+        With legal authorities when required by law
+        In the event of a business transfer or sale of our Website
+        With your consent
+        Security
+        We implement reasonable security measures to protect your personal information from unauthorized access or
+        disclosure. However, no method of transmission over the internet is entirely secure, and we cannot guarantee
+        absolute security.
+    </p>
+    <h2>
+        Your Choices
+    </h2>
+    <p>
+        You have the right to:
+
+        Access, update, or delete your personal information
+        Opt-out of receiving marketing communications
+        Control cookie settings
+    </p>
+    <h2>
+        Third-Party Links
+    </h2>
+    <p>
+        Our Website may contain links to third-party websites. We are not responsible for the privacy practices or
+        content
+        of these third-party websites. Please review their privacy policies before using their services.
+    </p>
+    <h2>
+        Children's Privacy
+    </h2>
+    <p>
+        Our Website is not intended for children under the age of 13. We do not knowingly collect or store information
+        from
+        individuals under 13 years old.
+    </p>
+    <h2>
+        Changes to this Privacy Policy
+    </h2>
+    <p>
+        We may update this Privacy Policy to reflect changes in our practices or legal requirements. We will notify you
+        of
+        any material changes by posting the updated policy on our Website.
+    </p>
+
+    <h2>
+        Contact Us
+    </h2>
+    <p>
+        If you have questions or concerns about this Privacy Policy, please contact us at [Insert Contact Information].
+
+        By using our Website, you acknowledge that you have read and understood this Privacy Policy and agree to its
+        terms
+        and conditions.
+    </p>
+</body>
+
+</html>
